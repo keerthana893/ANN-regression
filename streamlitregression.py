1725ca596c4c68944f156d8790f6be6b8560bb45
keerthana893 <163987983+keerthana893@users.noreply.github.com>
2025-05-15 12:49:24 +0530

    Add files via upload

---
+++ streamlitregression.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+import pandas as pd
+import pickle
+
+# load the trained model
+model=tf.keras.models.load_model('regression_model.keras')
+model.compile(optimizer='adam', loss='mse')
+
+# load the encoders and scaler
+with open('label_encoder_gender.pkl','rb')as file:
+    label_encoder_gender = pickle.load(file)
+    
+with open('onehot_encoder_geo.pkl','rb')as file:    
+    onehot_encoder_geo=pickle.load(file)
+
+with open('scaler.pkl','rb')as file:
+    scaler=pickle.load(file)
+
+# streamlit app   
+st.title('Estimated Salary Prediction')
+
+# user input
+geography=st.selectbox('geography',onehot_encoder_geo.categories_[0])
+gender=st.selectbox('Gender',label_encoder_gender.classes_)
+age=st.slider('Age',18,92) 
+balance=st.number_input('Balance')
+credit_score=st.number_input('Credit Score')   
+exited=st.selectbox('Exited',[0,1])
+tenure=st.slider('Tenure',0,10)
+num_of_products=st.slider('Number of Products',1,4)
+has_cr_card=st.selectbox('Has credit card',[0,1])
+is_active_member=st.selectbox('Is Active Member',[0,1])
+
+# prepare the input data--taking the input in the form of dictionary
+# converting the dictionary into data frame
+
+
+input_data=pd.DataFrame({
+   'CreditScore':[credit_score],
+   'Gender':[label_encoder_gender.transform([gender])[0]],
+   'Age':[age],
+   'Tenure':[tenure],
+   'Balance':[balance],
+   'NumOfProducts':[num_of_products],
+   'HasCrCard':[has_cr_card],
+   'IsActiveMember':[is_active_member],
+   'Exited': [exited] 
+})
+
+# in getting the input only geography is not included- so we are doing it with encoding
+# one hot encode - geography
+geo_encoded=onehot_encoder_geo.transform([['Geography']]).toarray()
+geo_encoded_df=pd.DataFrame(geo_encoded,columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+# combining one hot encode with input data
+
+input_data=pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+
+# scaling teh input data
+input_data_scaled=scaler.transform(input_data)
+
+#predict estimated salary
+
+prediction=model.predict(input_data_scaled)
+prediction_salary=prediction[0][0]
+
+st.write(f'Predicted Estimated Salary: ${prediction_salary:.2f}')
+
